96aa8302f8bc7efdef1a25a97bead935e40a37a4
Kobzar Vladislav <kobzarvllv@gmail.com>
2024-04-30 21:21:51 +0300

    add files

--- LightDM-Mini_Greeter/display_setup.sh
+++ LightDM-Mini_Greeter/display_setup.sh
@@ -3,7 +3,8 @@ xrandr --setprovideroutputsource modesetting NVIDIA-0
 # xrandr --auto
 xrandr --dpi 96
 if [[ $(xrandr -q | grep 'HDMI-1-2 connected') ]]; then
-	xrandr --output eDP-1-1 --primary --auto --output HDMI-1-2 --auto --right-of eDP-1-1
+	xrandr --output eDP-1-1 --off --primary --auto --output HDMI-1-2 --auto --right-of eDP-1-1
 else
-	xrandr --output eDP-1-1 --primary --auto --output HDMI-1-2 --off
+	xrandr --output eDP-1-1 --off --primary --auto --output HDMI-1-2 --off
 fi
+
